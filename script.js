bdd9d7373eccf6fe6148346a56848e5411ecc48c
Lazerlight <snndsnns2@gmail.com>
2023-03-05 21:13:05 +0200

    Update-05.2

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 import { movement } from "./player.js";
-import {} from "./spear.js";
+import { moveSpear, setupSpear } from "./spear.js";
 
 const playerEl = document.querySelector(".humanoid");
 const spearEl = document.querySelector(".spear");
 const messageEl = document.querySelector(".starting-message");
 
 let lastTime;
-
 function renderGame(time) {
   // The main function that constantly updates the game and renders the elements
   if (lastTime == null) {
@@ -17,7 +16,9 @@ function renderGame(time) {
   const delta = time - lastTime;
   lastTime = time;
   movement(delta);
+  moveSpear(delta);
   window.requestAnimationFrame(renderGame);
 }
+setupSpear();
 
 window.requestAnimationFrame(renderGame);
--- spear.js
+++ spear.js
@@ -1,17 +1,29 @@
 import { getProperty, incProperty, setProperty } from "./CustomFunctions.js";
 
 const SPEED = 0.1;
-const spearEl = document.querySelector(".spear");
+const mainEl = document.querySelector("main");
+let spearEls = document.querySelectorAll(".spear");
 
-export function setupSpear() {
+export function setupSpear(delta) {
   //  Creating and setting up the position of the spear element
+  spearEls.forEach((e) => {
+    e.style.left = getRandomPositionLeft() + "%";
+    setProperty(e, "--top", getRandomPositionTop());
+  });
 }
 
-export function moveSpear() {
+export function moveSpear(delta) {
   // Handling the spear movement and position
+  spearEls.forEach((e) => {
+    incProperty(e, "--top", SPEED * delta);
+  });
 }
 
-function getRandomPosition() {
+function getRandomPositionLeft() {
   // Returning a random number between 1 - 100
   return Math.floor(Math.random() * 100 + 1);
+}
+function getRandomPositionTop() {
+  // Returning a random number between -10 - -50
+  return Math.floor(Math.random() * -300 - 10);
 }
