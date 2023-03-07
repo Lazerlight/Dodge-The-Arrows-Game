4878045edc8089b5bd194c32ebbd2760c0cc7f79
Lazerlight <snndsnns2@gmail.com>
2023-03-07 14:29:49 +0200

    Update-06.2.1

--- background.js
+++ background.js
@@ -5,11 +5,13 @@ const backgroundEls = document.querySelectorAll(".background");
 const SPEED = 0.05;
 
 export function setupBackground() {
+  // Sets the starting position of the background elements
   setProperty(backgroundEls[0], "--top", 0);
   setProperty(backgroundEls[1], "--top", -100);
 }
 
 export function moveBackground(delta) {
+  // Constantly incrementing the position in this case the css variable --top and checking whether the background element should be looped or not
   backgroundEls.forEach((e) => {
     incProperty(e, "--top", SPEED * delta);
 
