25419b0a68df6590e5170b9755dc560b3d81df22
Lazerlight <snndsnns2@gmail.com>
2023-03-11 17:29:43 +0200

    Update-10

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 import { getProperty, incProperty, setProperty } from "./CustomFunctions.js";
+import { warpSpeed } from "./script.js";
 
 const backgroundEls = document.querySelectorAll(".background");
 
-const SPEED = 0.05;
+export let SPEED = 0.05;
 
 export function setupBackground() {
   // Sets the starting position of the background elements
@@ -12,9 +13,13 @@ export function setupBackground() {
 
 export function moveBackground(delta) {
   // Constantly incrementing the position in this case the css variable --top and checking whether the background element should be looped or not
+  if (warpSpeed) {
+    SPEED = 0.35;
+  } else {
+    SPEED = 0.05;
+  }
   backgroundEls.forEach((e) => {
     incProperty(e, "--top", SPEED * delta);
-
     if (getProperty(e, "--top") >= 100) {
       incProperty(e, "--top", -200);
     }
--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+export let warpSpeed = false;
+
 import { moveBackground, setupBackground } from "./background.js";
 import { movement } from "./player.js";
 import { moveSpear, setupSpear } from "./spear.js";
 import { moveWarp, setupWarp } from "./warp.js";
 
 const playerEl = document.querySelector(".humanoid");
-const spearEl = document.querySelector(".spear");
+const spearEl = document.querySelectorAll(".spear");
+const warpEl = document.querySelector(".warp");
 const messageEl = document.querySelector(".starting-message");
 
 let lastTime;
 let gameStarted = false;
+let warpSpeedTimeout = 5000;
 
 setupBackground();
 setupSpear();
@@ -38,8 +42,50 @@ function renderGame(time) {
     moveBackground(delta);
     moveSpear(delta);
     moveWarp(delta);
-
+    if (hasCollided(playerEl, spearEl)) {
+      gameStarted = false;
+    }
+    if (hasCollided(playerEl, warpEl)) {
+      handleWarpSpeed();
+    }
     window.requestAnimationFrame(renderGame);
   }
+}
+
+function hasCollided(a, b) {
+  const rect1 = a.getBoundingClientRect();
+  let collisionDetected = false;
+  if (NodeList.prototype.isPrototypeOf(b)) {
+    b.forEach((element) => {
+      const rect2 = element.getBoundingClientRect();
+      const collision = !(
+        rect1.right < rect2.left - 15 ||
+        rect1.left > rect2.right - 15 ||
+        rect1.bottom < rect2.top - 15 ||
+        rect1.top > rect2.bottom - 15
+      );
+
+      if (collision) {
+        collisionDetected = true;
+      }
+    });
+  } else {
+    const rect2 = b.getBoundingClientRect();
+
+    return !(
+      rect1.right < rect2.left - 15 ||
+      rect1.left > rect2.right - 15 ||
+      rect1.bottom < rect2.top - 15 ||
+      rect1.top > rect2.bottom - 15
+    );
+  }
+  return collisionDetected;
+}
+
+function handleWarpSpeed() {
+  warpSpeed = true;
+  setTimeout(() => {
+    warpSpeed = false;
+  }, warpSpeedTimeout);
 }
 window.requestAnimationFrame(renderGame);
