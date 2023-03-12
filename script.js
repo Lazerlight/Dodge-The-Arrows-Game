63f2f4b8b27e063948aa1f2fab710dcc04666425
Lazerlight <snndsnns2@gmail.com>
2023-03-12 17:13:14 +0200

    Update-11

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const playerEl = document.querySelector(".humanoid");
 const spearEl = document.querySelectorAll(".spear");
 const warpEl = document.querySelector(".warp");
 const messageEl = document.querySelector(".starting-message");
+const loseMessageEl = document.querySelector(".lose-message");
+const buttonEl = document.querySelector("button");
 
 let lastTime;
 let gameStarted = false;
@@ -27,7 +29,9 @@ window.addEventListener(
   },
   { once: true }
 );
-
+buttonEl.addEventListener("click", () => {
+  window.location.reload();
+});
 function renderGame(time) {
   // The main function that constantly updates the game and renders the elements
   if (gameStarted) {
@@ -44,6 +48,7 @@ function renderGame(time) {
     moveWarp(delta);
     if (hasCollided(playerEl, spearEl)) {
       gameStarted = false;
+      loseMessageEl.classList.remove("hidden");
     }
     if (hasCollided(playerEl, warpEl)) {
       handleWarpSpeed();
@@ -59,8 +64,8 @@ function hasCollided(a, b) {
     b.forEach((element) => {
       const rect2 = element.getBoundingClientRect();
       const collision = !(
-        rect1.right < rect2.left - 15 ||
-        rect1.left > rect2.right - 15 ||
+        rect1.right < rect2.left + 15 ||
+        rect1.left > rect2.right + 15 ||
         rect1.bottom < rect2.top - 15 ||
         rect1.top > rect2.bottom - 15
       );
